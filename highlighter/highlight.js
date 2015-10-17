80767099f569a534a5fd9487bb82fea471033884
Misato Morino <mimaunxxx@gmail.com>
2015-10-17 16:52:17 -0400

    Almost done, but some words ",.!?()" includes, it should be fixed

--- highlighter/highlight.js
+++ highlighter/highlight.js
@@ -5,30 +5,86 @@ window.onload = function() {
 
     // get all words from <p>.
     var allText = $("p").text();
-    var allwords = allText.split(/[,.\n\s!?()]+/);
-    var spanList = [];
+    $("p").replaceWith("");
+    var allLines= allText.split(/[\n]+/);
+    var allwords = []
+    
+    $.each(allLines, function(i, val) { 
+        // var wordArr = val.split(/[,.\s!?()]+/);
+        var wordArr = val.split(/[\s!?()]+/);
+        $.each(wordArr, function(i, val) { 
+            if(val != undefined) 
+                allwords.push(val);
+        });
+        allwords.push("\n");
+    });
 
-    // add class, class's name should be same each lowercase word.
+    //add class, class's name should be same each lowercase word.
     $.each(allwords, function(i, val) { 
-
+    
+        var newSpan;
+    
         // avoid null.
         if(val != undefined) {
-            var newSpan = $('<span>' + val + '</span>');
+            newSpan = $("<span>" + val + "</span>");
             newSpan.addClass(val.toLowerCase());
-            spanList.push(newSpan);
+            newSpan.css({
+                "background-color": "transparent"
+            });
+
+            // click event.
+            newSpan.click(clickWord);
+
+            // print to screen.
+            if(val == "\n") {
+                $("body").append("<br>");
+                return true;
+            }
+            // else if(val == "," || val == "." || val == "?" || val == "!") {
+            //     $("body").append(val);
+            //     return true;
+            // }
+            else {
+                $("body").append(newSpan); 
+                $("body").append(" "); 
+            }
         } 
         // delete null.
         else 
-            allwords.splice(i, 1);
+            allwords.splice(i, 1); 
     }); 
+};
 
-    // click event.
+// click event.
+function clickWord() {
+
+    // search word.
+    var word = $(this).attr("class");
 
     // remove highlight class from all words.
+    $("span").each(function(i) {
+        if($(this).hasClass("highlight")) {
+
+            $(this).removeClass("highlight");
+
+            // reset highlight color.
+            $(this).css({
+                "background-color": "transparent"
+            });
+        }
+    });
 
     // search same word by class name.
-    
-    // create and add "highlight" class, it should highlight word.
+    $("span").each(function(i) { 
+        if($(this).attr("class") == word) {
+            $(this).addClass("highlight");
+            console.log("found");
+        }
+    });
 
-};
+    // create and add "highlight" class, it should highlight word.
+    $("span.highlight").css({
+        "background-color": "lightgreen"
+    });
+}
 
