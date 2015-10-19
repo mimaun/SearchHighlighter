bfe22ecd4d0cb00b1ca75a682772ae39ec56987c
Misato Morino <mimaunxxx@gmail.com>
2015-10-18 22:19:56 -0400

    deal with special characters

--- highlighter/highlight.js
+++ highlighter/highlight.js
@@ -1,17 +1,17 @@
 "use strict";
 
 window.onload = function() { 
-// fill in this anonymous function's  body and add other functions as needed.
+    // fill in this anonymous function's  body and add other functions as needed.
 
     // get all words from <p>.
     var allText = $("p").text();
     $("p").replaceWith("");
     var allLines= allText.split(/[\n]+/);
     var allwords = []
-    
+
     $.each(allLines, function(i, val) { 
         // var wordArr = val.split(/[,.\s!?()]+/);
-        var wordArr = val.split(/[\s!?()]+/);
+        var wordArr = val.split(/[\s]+/);
         $.each(wordArr, function(i, val) { 
             if(val != undefined) 
                 allwords.push(val);
@@ -19,15 +19,55 @@ window.onload = function() {
         allwords.push("\n");
     });
 
-    //add class, class's name should be same each lowercase word.
+    // add class, class's name should be same each lowercase word.
     $.each(allwords, function(i, val) { 
-    
+
         var newSpan;
-    
+        var isSplitCharAtEnd = false;
+
         // avoid null.
         if(val != undefined) {
-            newSpan = $("<span>" + val + "</span>");
-            newSpan.addClass(val.toLowerCase());
+
+            // escape ,.!?()'":;
+            var checkCharPosition = val.search(/[\\,\\.\\!\\?\\(\\)\\'\\"\\:\\;]/);
+            var splitChar = ""
+            
+            // no escape key.
+            if(checkCharPosition == -1) {
+                newSpan = $("<span>" + val + "</span>");
+                newSpan.addClass(val.toLowerCase());
+                isSplitCharAtEnd = false;
+            } 
+            
+            // found escape key.
+            else {
+                splitChar = val.charAt(checkCharPosition);
+                var lengthOfWord = val.length;
+                var myWord = "";
+
+                // begin.
+                if(checkCharPosition == 0) {
+                    myWord = val.slice(1);
+                    $("body").append(splitChar); 
+                    newSpan = $("<span>" + myWord + "</span>");
+                    newSpan.addClass(myWord.toLowerCase());
+                    isSplitCharAtEnd = false;
+                }
+                // end.
+                else if(checkCharPosition == lengthOfWord-1) {
+                    myWord = val.slice(0, lengthOfWord-1);
+                    newSpan = $("<span>" + myWord + "</span>");
+                    newSpan.addClass(myWord.toLowerCase());
+                    isSplitCharAtEnd = true;
+                }
+                // other
+                else { 
+                    newSpan = $("<span>" + val + "</span>");
+                    newSpan.addClass(val.toLowerCase());
+                    isSplitCharAtEnd = false;
+                }
+            }
+
             newSpan.css({
                 "background-color": "transparent"
             });
@@ -40,23 +80,29 @@ window.onload = function() {
                 $("body").append("<br>");
                 return true;
             }
-            // else if(val == "," || val == "." || val == "?" || val == "!") {
-            //     $("body").append(val);
-            //     return true;
-            // }
             else {
                 $("body").append(newSpan); 
+                if(isSplitCharAtEnd)
+                    $("body").append(splitChar); 
                 $("body").append(" "); 
+                isSplitCharAtEnd = false;
             }
         } 
         // delete null.
         else 
             allwords.splice(i, 1); 
     }); 
+
+    console.log("I've done.");
 };
 
 // click event.
 function clickWord() {
+    console.log("clickEvent");
+
+    /*
+     *  $(“[class =^ Mary]”).css(color:red)
+     * */
 
     // search word.
     var word = $(this).attr("class");
@@ -86,5 +132,7 @@ function clickWord() {
     $("span.highlight").css({
         "background-color": "lightgreen"
     });
+
+    console.log("I've done.");
 }
 
